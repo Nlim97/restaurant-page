5bec10713e57b8135bb353045d1c7a017d66757d
Nlim97 <nobellimbu1@gmail.com>
2024-09-16 18:43:42 +0100

    created menu elements and implemented logic for navigation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import { header } from "./header";
 import { hero } from "./hero";
 import { location } from "./location";
 import { footer } from "./footer";
+import { menu } from "./menu";
 import "./styles.css"
 
-header();
-hero();
+document.addEventListener("DOMContentLoaded", () => {
+const heroElement = hero()
+const menuElement = menu()
+header(heroElement, menuElement);
+document.body.appendChild(heroElement)
 location();
 footer();
+
+})
